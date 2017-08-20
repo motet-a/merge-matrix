f5d025e1b825567ac2d7c8df771d9145b295cfb3
Antoine Motet <antoine.motet@gmail.com>
2017-08-20 23:35:09 +0200

    Compress merge logs

--- src/merge-logs.js
+++ src/merge-logs.js
@@ -2,6 +2,7 @@
 const {promisify} = require('util')
 const fs = require('fs')
 const path = require('path')
+const zlib = require('zlib')
 
 const {fileExists, ensureVarDir} = require('./util')
 
@@ -16,18 +17,19 @@ const save = async results => {
 
     for (result of results) {
         const fileName = result.aSha + '-' + result.bSha
+        const compressed = await promisify(zlib.gzip)(JSON.stringify(result))
         await promisify(fs.writeFile)(
             path.join(dirPath, fileName),
-            JSON.stringify(result),
+            compressed,
         )
     }
 }
 
 const get = async (aSha, bSha) => {
     const filePath = path.join(dirPath, aSha + '-' + bSha)
-    let buffer
+    let compressed
     try {
-        buffer = await promisify(fs.readFile)(filePath)
+        compressed = await promisify(fs.readFile)(filePath)
     } catch (error) {
         if (error.code === 'ENOENT') {
             return null
@@ -35,6 +37,7 @@ const get = async (aSha, bSha) => {
 
         throw error
     }
+    const buffer = await promisify(zlib.gunzip)(compressed)
     return JSON.parse(buffer.toString())
 }
 
