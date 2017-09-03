0d59e8e9cc1babb7590b2719bf3df0d97d0a090a
Antoine Motet <antoine.motet@gmail.com>
2017-09-03 07:08:37 +0200

    git: Increase output buffer to 2MB
    The default `maxBuffer` is 200KB.

--- src/git.js
+++ src/git.js
@@ -25,7 +25,10 @@ const git = async (args, options) => {
             'git',
             args,
             Object.assign(
-                {env},
+                {
+                    env,
+                    maxBuffer: 2 * 1024 * 1024,
+                },
                 options,
             ),
         )
